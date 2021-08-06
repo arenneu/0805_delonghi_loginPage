80480cb35ee2c88943d1d94977d6382d7215cf17
arenneu <jisusjy@naver.com>
2021-08-06 15:46:18 +0900

    0806_GNB작업중(서브&배경디버깅중)

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+    $(".mainLi").mouseover(function () {
+        $(this).children("ul").stop().show();
+        $("#subMenuBG").stop().show();
+    });
+    $(".mainLi").mouseout(function () {
+        $(this).children("ul").stop().hide();
+        $("#subMenuBG").stop().hide();
+    });
 });
